5bc6ff804a5c0d76c770e5b05783b7542bd774d7
Vadim1044 <67069732+Vadim1044@users.noreply.github.com>
2024-10-01 10:34:35 +0600

    Add files via upload

---
+++ ДЗ за 27.09.24/zadacha3.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class HelloWorld{
+    public static void Main(string[] args){
+        int n = Convert.ToInt32(Console.ReadLine());
+        int a = 0;
+        int b = 0;
+        int max = 0;
+        for (int i=0; i<n; i++){
+            int g = Convert.ToInt32(Console.ReadLine());
+            if (g>max){
+                a=max;
+                max=g;
+            }
+        }
+    Console.WriteLine(a);
+    }
+    
+}
---
+++ ДЗ за 27.09.24/zadaha1.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class HelloWorld{
+    public static void Main(string[] args){
+        int n = Convert.ToInt32(Console.ReadLine());
+        int a = 0;
+        int b = 0;
+        int min = 0;
+        for (int i=0; i<n; i++){
+            int g = Convert.ToInt32(Console.ReadLine());
+            if (i == 0){
+                a=g;
+            }
+            else if (i == 1){
+                b=g;
+            }
+            if (i<n && i>1){
+                if(g>b && a>b){
+                    min+=1;
+                }
+            }
+        }
+    Console.WriteLine(min);
+    }
+    
+}
