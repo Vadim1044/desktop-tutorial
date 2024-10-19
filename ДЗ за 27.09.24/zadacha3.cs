c13585a5d48ae098b190dc20cd0a5693a75f1134
Vadim1044 <67069732+Vadim1044@users.noreply.github.com>
2024-10-19 12:47:58 +0600

    Update zadacha3.cs

--- ДЗ за 27.09.24/zadacha3.cs
+++ ДЗ за 27.09.24/zadacha3.cs
@@ -3,16 +3,19 @@ using System;
 public class HelloWorld{
     public static void Main(string[] args){
         int n = Convert.ToInt32(Console.ReadLine());
-        int a = 0;
-        int max = 0;
+        int max_second = 0;
+        int max_first = 0;
         for (int i=0; i<n; i++){
             int g = Convert.ToInt32(Console.ReadLine());
-            if (g>max){
-                a=max;
-                max=g;
+            if (g>max_first){
+                max_second=max_first;
+                max_first=g;
+            }
+            else if(g>max_second){
+                max_second=g;
             }
         }
-    Console.WriteLine(a);
+    Console.WriteLine(max_second);
     }
     
 }
