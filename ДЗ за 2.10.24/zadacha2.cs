7cf3df2aa7814c1b341ed831b298791342607b6b
Vadim1044 <67069732+Vadim1044@users.noreply.github.com>
2024-10-30 13:53:06 +0600

    Update zadacha2.cs

--- ДЗ за 2.10.24/zadacha2.cs
+++ ДЗ за 2.10.24/zadacha2.cs
@@ -21,6 +21,7 @@ public static class Program
                 b=0;
             }
         } 
+        Console.WriteLine("Минимальная длина последовательности четных элементов:");
         Console.WriteLine(min);
     } 
 }
