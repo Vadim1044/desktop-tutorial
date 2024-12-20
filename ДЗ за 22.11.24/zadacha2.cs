93a9836d5c1d89d53ee688c6f018272baea4610c
Vadim1044 <67069732+Vadim1044@users.noreply.github.com>
2024-12-20 20:46:37 +0600

    Update zadacha2.cs

--- ДЗ за 22.11.24/zadacha2.cs
+++ ДЗ за 22.11.24/zadacha2.cs
@@ -5,40 +5,28 @@ public class Program
 {
     public static void Main() 
     {
-        int [][] array = new int [3][];
-        array[0] = new int[5];
-        array[1] = new int[5];
-        array[2] = new int[3];
-        array[0] = new int[0] {1,3,5,7,9};
-        array[1] = new int[0] {0,2,4,6,50};
-        array[2] = new int[0] {11,22,13};
-        
+        Console.WriteLine("Введите оклво массивов в массиве");
+        int n = int.Parse(Console.ReadLine());
+        int [][] array = new int [n][];
+        for (int i = 0; i<n; i++){
+            Console.WriteLine("Введите колво элементов массива №" + i);
+            int g = int.Parse(Console.ReadLine());
+            array[i] = new int [g];
+            for ( int j = 0; j<g; j++){
+                Console.WriteLine("Введите элементы массива №" + i);
+                array[i][j] = int.Parse(Console.ReadLine());
+            }
+        }
         
         count(array);
-        output(array);
     }
-    // public static void output(int[,] array)
-    // {
-    //     int temp = 0;
-    //     for (int i = 0; i < array.GetLength(0); i++)
-    //     {
-    //         for (int j = 0; j < array.GetLength(1); j++)
-    //         {
-    //             Console.Write(array[i, j]);
-    //             if (j == (array.GetLength(1)-1)){
-    //                 Console.Write(",");
-    //             }
-    //         }
-    //     }
-    // }
 
-
-    public static void count(int[][] array){
+    public static void count(int[][] arr){
         
-        for (int i=0; i<array.GetLength(0); i++){
+        for (int i=0; i<arr.GetLength(0); i++){
             int sum = 0;
-            int Max = array[i][0];
-            for (int j=0; j<array[0].GetLength(0); j++){
+            int max = arr[i][0];
+            for (int j=0; j<arr[i].GetLength(0); j++){
                     
                 sum += arr[i][j];
                 if ( max < arr[i][j]){
