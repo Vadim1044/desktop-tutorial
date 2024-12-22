54ba9563aea38c5d9bf122efb1d1e3fc55413b14
Vadim1044 <67069732+Vadim1044@users.noreply.github.com>
2024-12-22 12:10:52 +0600

    Update zadacha2.cs

--- ДЗ за 2.10.24/zadacha2.cs
+++ ДЗ за 2.10.24/zadacha2.cs
@@ -1,67 +1,33 @@
-using System; 
- 
-public static class Program
-{
-    public static void Main() 
-    {
-        int n = Convert.ToInt32(Console.ReadLine());
-        int b = 0;
-        int max = n;
-        for (int i = 0; i < n; i++)
-        {
-            int g = Convert.ToInt32(Console.ReadLine());
-            if (g % 2 == 0)
-            {
-                b++;
-            }
-            else{
-                if (max>b){
-                    max=b;
-                }
-                b=0;
-            }
-            if (i == (n-1)){
-                if (max>b){
-                    max=b;
-                }
-                Console.WriteLine(max);
-            }
-        } 
-    } 
-}
-
-
-
 using System;
 class Second
 {
     static void Main()
     {
         int n = Convert.ToInt32(Console.ReadLine());
-        int minima = 9999999;
-        int minimaComparison = 0;
+        int min = 9999999;
+        int minComparison = 0;
         for (int i = 0; i < n; i++)
         {
             int a = Convert.ToInt32(Console.ReadLine());
             if (a % 2 == 0)
             {
-                minimaComparison++;
+                minComparison++;
             }
-            else if (minimaComparison != 0)
+            else if (minComparison != 0)
             {
-                minima = Math.Min(minima, minimaComparison);
-                minimaComparison = 0;
+                min = Math.Min(min, minComparison);
+                minComparison = 0;
             }
             else
             {
-                minimaComparison = 0;
+                minComparison = 0;
             }
         }
-        if (minimaComparison != 0)
+        if (minComparison != 0)
         {
-            minima = Math.Min(minima, minimaComparison);
+            min = Math.Min(min, minComparison);
 
         }
-        Console.WriteLine(minima);
+        Console.WriteLine(min);
     }
 }
