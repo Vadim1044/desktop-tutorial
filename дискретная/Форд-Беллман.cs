ad86adaac3c53dff9fcac00abb9f73a863899000
Vadim1044 <67069732+Vadim1044@users.noreply.github.com>
2025-04-07 09:24:41 +0600

    Add files via upload

---
+++ дискретная/Форд-Беллман.cs
@@ -0,0 +1,32 @@
+﻿def bellman_ford(num_vertices, edges, fisrt_vertice):
+    distances = [float('inf')] * num_vertices
+    distances[fisrt_vertice] = 0
+    for _ in range(num_vertices - 1):
+        for u, v, w in edges:
+            if distances[u] != float('inf') and distances[u] + w < distances[v]:
+                distances[v] = distances[u] + w
+    for u, v, w in edges:
+        if distances[u] != float('inf') and distances[u] + w < distances[v]:
+            print("граф содержит цикл с отрицательным весом")
+            return None
+
+    return distances
+
+if __name__ == "__main__":
+    num_vertices = 5
+    edges = [
+        (0, 1, 6),
+        (0, 2, 7),
+        (1, 2, 8),
+        (1, 3, -4),
+        (1, 4, 5),
+        (2, 3, 9),
+        (2, 4, -3),
+        (3, 4, 7)
+    ]
+    fisrt_vertice = 0
+
+    distances = bellman_ford(num_vertices, edges, fisrt_vertice)
+    print(f"расстояние от вершины {fisrt_vertice} до всех: ")
+    for i in range(num_vertices):
+            print(f"{i}\t\t{distances[i]}")
---
+++ дискретная/прима_краскал.cs
@@ -0,0 +1,45 @@
+﻿def find(parent, i):
+    if parent[i] == -1:
+        return i
+    return find(parent, parent[i])
+
+def union(parent, x, y):
+    parent[x] = y
+
+def prima_kruskal(vertices, edges):
+    edges.sort(key=lambda edge: edge[2])
+
+    parent = [-1] * vertices
+    minimum_cost = 0
+    result_edges = []
+
+    for edge in edges:
+        source, destination, weight = edge
+        x = find(parent, source)
+        y = find(parent, destination)
+
+        if x != y:
+            minimum_cost += weight
+            result_edges.append(edge)
+            union(parent, x, y)
+
+    print("Вес оставного дерева:", minimum_cost)
+    print("Рёбра остовного дерева:")
+    for edge in result_edges:
+        print(f"({edge[0]} -- {edge[1]}, вес: {edge[2]})")
+
+def main():
+    vertices = 4
+    edges = [
+        (0, 1, 10),
+        (0, 2, 6),
+        (0, 3, 5),
+        (1, 3, 15),
+        (2, 3, 4)
+    ]
+
+    prima_kruskal(vertices, edges)
+
+if __name__ == "__main__":
+    main();
+
